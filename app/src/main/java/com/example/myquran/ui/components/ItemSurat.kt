9566dbe42be74ba05cd8c5385335f014ea40752d
nopal72 <117352944+nopal72@users.noreply.github.com>
2025-03-25 21:05:34 +0700

    benerin background di ItemSurat

--- app/src/main/java/com/example/myquran/ui/components/ItemSurat.kt
+++ app/src/main/java/com/example/myquran/ui/components/ItemSurat.kt
@@ -11,23 +11,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.myquran.ui.theme.GreenMint
-import com.example.myquran.ui.theme.GreenNavy
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import com.example.myquran.ui.theme.GreenArmy
+import com.example.myquran.ui.theme.GreenBase
 import androidx.compose.ui.text.font.FontWeight.Companion as FontWeight1
 
 @Composable
@@ -37,10 +33,10 @@ fun ItemSurat() {
             .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 8.dp)
             .background(
-                brush = Brush.horizontalGradient(
+                brush = Brush.verticalGradient(
                     colors = listOf(
-                        Color(0xFF1A5F38),  // Hijau tua di sebelah kiri
-                        Color(0xFF2C7744)   // Hijau muda di sebelah kanan
+                        GreenBase,
+                        GreenArmy
                     )
                 ),
                 shape = RoundedCornerShape(12.dp)
