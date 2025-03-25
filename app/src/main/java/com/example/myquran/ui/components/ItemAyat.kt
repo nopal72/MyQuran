e456a112661517c36b4f37e5ca387967e262dfb5
nopal72 <117352944+nopal72@users.noreply.github.com>
2025-03-25 21:37:55 +0700

    nambah icon bookmark

--- app/src/main/java/com/example/myquran/ui/components/ItemAyat.kt
+++ app/src/main/java/com/example/myquran/ui/components/ItemAyat.kt
@@ -7,17 +7,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.myquran.R
 import com.example.myquran.ui.theme.GreenArmy
 import com.example.myquran.ui.theme.GreenBase
 
@@ -76,6 +80,14 @@ fun ItemAyat(){
                 text = "Dengan nama Allah Yang Maha Pengasih, Maha Penyayang.",
                 color = Color.White.copy(alpha = 0.7f),
                 fontSize = 12.sp,
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.bookmark),
+                contentDescription = "bookmark",
+                tint = Color.White,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .size(28.dp)
             )
         }
     }
